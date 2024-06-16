faec0cc20a224c35328b50a944ea618d8bbc9ae5
sbx_user1051 <sbx_user1051@169.254.132.45>
2024-06-16 13:54:55 +0000

    Sun Jun 16 13:54:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,12 @@
-
 from collections import Counter
-import typing as t 
+import typing as t
 from dataclasses import dataclass
 
 # -----------------------
 # ---- REQUIREMENT 1 ----
 # -----------------------
 
-# Our price table and offers: 
+# Our price table and offers:
 # +------+-------+----------------+
 # | Item | Price | Special offers |
 # +------+-------+----------------+
@@ -26,18 +25,18 @@ from dataclasses import dataclass
 #         if item == "A":
 #             num_bundles = quantity // 3
 #             num_leftover =  quantity % 3
-#             total_cost += (num_leftover * 50) + (num_bundles * 130) 
+#             total_cost += (num_leftover * 50) + (num_bundles * 130)
 #         elif item == "B":
 #             num_bundles = quantity // 2
 #             num_leftover =  quantity % 2
-#             total_cost += (num_leftover * 30) + (num_bundles * 45)         
+#             total_cost += (num_leftover * 30) + (num_bundles * 45)
 #         elif item == "C":
 #             total_cost += 20 * quantity
 #         elif item == "D":
 #             total_cost += 15 * quantity
 #         else:
-#             return -1 
-    
+#             return -1
+
 #     return total_cost
 
 
@@ -45,7 +44,7 @@ from dataclasses import dataclass
 # ---- REQUIREMENT 2 ----
 # -----------------------
 
-# Our price table and offers: 
+# Our price table and offers:
 # +------+-------+------------------------+
 # | Item | Price | Special offers         |
 # +------+-------+------------------------+
@@ -59,15 +58,22 @@ from dataclasses import dataclass
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-def discounted_price_calculator(quantity: int, original_price: int, num_required_for_discount: int, discount_bundle_price: int) -> int:
+
+def discounted_price_calculator(
+    quantity: int,
+    original_price: int,
+    num_required_for_discount: int,
+    discount_bundle_price: int,
+) -> int:
     num_bundles = quantity // num_required_for_discount
-    num_leftover =  quantity % num_required_for_discount
+    num_leftover = quantity % num_required_for_discount
     return (num_leftover * original_price) + (num_bundles * discount_bundle_price)
+
 
 # def checkout(sku: str) -> int:
 #     counts = Counter(sku) # returns a sorted dictionary of counts
 #     total_cost = 0
-    
+
 #     for item, quantity in counts.items():
 #         if item == "A":
 #             # 5 multi discount (and possibly the 3 multi discount as well), its always better to use the 5 deal if you can
@@ -84,8 +90,8 @@ def discounted_price_calculator(quantity: int, original_price: int, num_required
 #         elif item == "E": # price E separately to the E logic in the B conditional as we always need to pay for anyway
 #             total_cost += 40 * quantity
 #         else:
-#             return -1 
-    
+#             return -1
+
 #     return total_cost
 
 
@@ -93,7 +99,7 @@ def discounted_price_calculator(quantity: int, original_price: int, num_required
 # ---- REQUIREMENT 3 ----
 # -----------------------
 
-# Our price table and offers: 
+# Our price table and offers:
 # +------+-------+------------------------+
 # | Item | Price | Special offers         |
 # +------+-------+------------------------+
@@ -106,31 +112,47 @@ def discounted_price_calculator(quantity: int, original_price: int, num_required
 # +------+-------+------------------------+
 
 
-
 def checkout(sku: str) -> int:
-    counts = Counter(sku) # returns a sorted dictionary of counts
+    counts = Counter(sku)  # returns a sorted dictionary of counts
     total_cost = 0
-    
+
     for item, quantity in counts.items():
         if item == "A":
             # 5 multi discount (and possibly the 3 multi discount as well), its always better to use the 5 deal if you can
             suggested_cost = (quantity // 5) * 200
-            suggested_cost += discounted_price_calculator(quantity=quantity % 5, original_price=50, num_required_for_discount=3, discount_bundle_price=130)
+            suggested_cost += discounted_price_calculator(
+                quantity=quantity % 5,
+                original_price=50,
+                num_required_for_discount=3,
+                discount_bundle_price=130,
+            )
             total_cost += suggested_cost
         elif item == "B":
             # always better to get a B free then possibly use the 2 multi discount
-            total_cost += discounted_price_calculator(quantity=quantity - counts.get('E', 0) // 2, original_price=30, num_required_for_discount=2, discount_bundle_price=45)
+            total_cost += discounted_price_calculator(
+                quantity=quantity - counts.get("E", 0) // 2,
+                original_price=30,
+                num_required_for_discount=2,
+                discount_bundle_price=45,
+            )
         elif item == "C":
             total_cost += 20 * quantity
         elif item == "D":
             total_cost += 15 * quantity
-        elif item == "E": # price E separately to the E logic in the B conditional as we always need to pay for anyway
+        elif (
+            item == "E"
+        ):  # price E separately to the E logic in the B conditional as we always need to pay for anyway
             total_cost += 40 * quantity
         elif item == "F":
-            total_cost += discounted_price_calculator(quantity=quantity, original_price=10, num_required_for_discount=3, discount_bundle_price=20)
+            total_cost += discounted_price_calculator(
+                quantity=quantity,
+                original_price=10,
+                num_required_for_discount=3,
+                discount_bundle_price=20,
+            )
         else:
-            return -1 
-    
+            return -1
+
     return total_cost
 
 
@@ -169,36 +191,50 @@ def checkout(sku: str) -> int:
 # | Z    | 50    |                        |
 # +------+-------+------------------------+
 
+
 @dataclass
 class FreeItemDeal:
     item_to_buy: str
-    required_quantity: int 
+    required_quantity: int
+
 
 @dataclass(order=True)
 class BundleDeal:
-    size: int 
-    bundle_price: int 
+    size: int
+    bundle_price: int
+
 
 class ItemPricing:
-    def __int__(self, item: str, original_price: int, bundle_deals: t.Optional[t.List[BundleDeal]] = None, free_item_deal: t.Optional[FreeItemDeal]= None):
+    def __int__(
+        self,
+        item: str,
+        original_price: int,
+        bundle_deals: t.Optional[t.List[BundleDeal]] = None,
+        free_item_deal: t.Optional[FreeItemDeal] = None,
+    ):
         self.item = item
         self.item_original_price = original_price
         self.bundle_deals = bundle_deals
         self.free_item_deal = free_item_deal
-    
+
     def calculate_cost(self, quantities: t.Dict[str, int]) -> int:
         quantity = quantities[self.item]
         if self.free_item_deal:
-            quantity -= quantities.get(self.free_item_deal.item_to_buy, 0) // self.free_item_deal.required_quantity
+            quantity -= (
+                quantities.get(self.free_item_deal.item_to_buy, 0)
+                // self.free_item_deal.required_quantity
+            )
 
         cost = 0
         if self.bundle_deals:
             for deal in sorted(self.bundle_deals):
                 num_bundles = quantity // deal.size
                 cost += num_bundles * deal.bundle_price
-                quantity =  quantity % deal.size
+                quantity = quantity % deal.size
 
-        return cost + (quantity * self.item_original_price) # TODO this only works if the deal with a bigger size is better price per item?
+        return cost + (
+            quantity * self.item_original_price
+        )  # TODO this only works if the deal with a bigger size is better price per item?
 
 
 # +------+-------+------------------------+
@@ -236,20 +272,31 @@ class ItemPricing:
 def checkout(sku: str) -> int:
     pricing_mapping = {
         "A": ItemPricing("A", 50, [BundleDeal(3, 130), BundleDeal(5, 200)]),
-        "B": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
-        "C": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
-        "D": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
-        "E": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
+        "B": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "C": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "D": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "E": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
         "F": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
         "G": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
         "H": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
         "I": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
-        "J": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
-        "K": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()), 
-        "L", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-
-
-
+        "J": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "K": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "L": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "N": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "O": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "P": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "Q": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "R": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "S": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "T": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "U": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "V": ItemPricing("B", 30, [BundleDeal(2, 45)], FreeItemDeal()),
+        "W": ItemPricing("W", 20),
+        "X": ItemPricing("X", 90),
+        "Y": ItemPricing("Y", 10),
+        "Z": ItemPricing("Z", 50),
+    }
 
     counts = Counter(sku)
     total_cost = 0
@@ -258,8 +305,9 @@ def checkout(sku: str) -> int:
         if item.isalpha():
             total_cost += pricing_mapping[item].calculate_cost(counts)
         else:
-            return -1 
+            return -1
     return total_cost
+
 
 
 
