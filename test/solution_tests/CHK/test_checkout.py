f1859ca0172b80820e66f1fc43a78af43d940b16
sbx_user1051 <sbx_user1051@169.254.132.45>
2024-06-16 09:16:41 +0000

    Sun Jun 16 09:16:41 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,43 +3,109 @@
 from solutions.CHK import checkout_solution
 
 
-# class TestCheckout():
-#     def test_A(self):
-#         # test item A pricing
-#         assert checkout_solution.checkout("A") == 50
+# -----------------------
+# ---- REQUIREMENT 1 ----
+# -----------------------
 
-#         # test item A multi (discount)
-#         assert checkout_solution.checkout("AAA") == 130
-#         assert checkout_solution.checkout("AAAAAA") == 260
-#         assert checkout_solution.checkout("AAAAAAAA") == 360
+class TestCheckoutR1(): # TODO should these still pass? Deal with them after write new tests
+    def test_A(self):
+        # test item A pricing
+        assert checkout_solution.checkout("A") == 50
 
-#     def test_B(self):
-#         # test item B pricing
-#         assert checkout_solution.checkout("B") == 30
+        # test item A multi (discount)
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("AAAAAA") == 260
+        assert checkout_solution.checkout("AAAAAAAA") == 360
 
-#         # test item B multi (discount)
-#         assert checkout_solution.checkout("BB") == 45
-#         assert checkout_solution.checkout("BBBB") == 90
-#         assert checkout_solution.checkout("BBBBB") == 120
-#     def test_C(self):
-#         # test item C pricing
-#         assert checkout_solution.checkout("C") == 20
+    def test_B(self):
+        # test item B pricing
+        assert checkout_solution.checkout("B") == 30
 
-#         # test item C multi (no discount)
-#         assert checkout_solution.checkout("CC") == 40
+        # test item B multi (discount)
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("BBBB") == 90
+        assert checkout_solution.checkout("BBBBB") == 120
+    def test_C(self):
+        # test item C pricing
+        assert checkout_solution.checkout("C") == 20
 
-#     def test_D(self):
-#         # test item D pricing
-#         assert checkout_solution.checkout("D") == 15
+        # test item C multi (no discount)
+        assert checkout_solution.checkout("CC") == 40
 
-#         # test item D multi (no discount)
-#         assert checkout_solution.checkout("DD") == 30
+    def test_D(self):
+        # test item D pricing
+        assert checkout_solution.checkout("D") == 15
+
+        # test item D multi (no discount)
+        assert checkout_solution.checkout("DD") == 30
     
-#     def test_multi(self):
-#         # test multiple items in the cart, including discounts on A and B
-#         # expected result: 130 + 45 + 40 + 30 
-#         assert checkout_solution.checkout("ABCDABCDA") == 245   
+    def test_multi(self):
+        # test multiple items in the cart, including discounts on A and B
+        # expected result: 130 + 45 + 40 + 30 
+        assert checkout_solution.checkout("ABCDABCDA") == 245   
         
-#     def test_invalid(self):
-#         # test an invalid cart
-#         assert checkout_solution.checkout("ABZCD") == -1
+    def test_invalid(self):
+        # test an invalid cart
+        assert checkout_solution.checkout("ABZCD") == -1
+
+
+# -----------------------
+# ---- REQUIREMENT 2 ----
+# -----------------------
+        
+# Our price table and offers: 
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+
+
+class TestCheckout():
+    def test_discounted_price_calculator():
+        assert checkout_solution.discounted_price_calculator()
+        
+    def test_A(self):
+        # test item A pricing
+        assert checkout_solution.checkout("A") == 50
+
+        # test item A multi (discount)
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("AAAAAA") == 260
+        assert checkout_solution.checkout("AAAAAAAA") == 360
+
+    def test_B(self):
+        # test item B pricing
+        assert checkout_solution.checkout("B") == 30
+
+        # test item B multi (discount)
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("BBBB") == 90
+        assert checkout_solution.checkout("BBBBB") == 120
+    def test_C(self):
+        # test item C pricing
+        assert checkout_solution.checkout("C") == 20
+
+        # test item C multi (no discount)
+        assert checkout_solution.checkout("CC") == 40
+
+    def test_D(self):
+        # test item D pricing
+        assert checkout_solution.checkout("D") == 15
+
+        # test item D multi (no discount)
+        assert checkout_solution.checkout("DD") == 30
+    
+    def test_multi(self):
+        # test multiple items in the cart, including discounts on A and B
+        # expected result: 130 + 45 + 40 + 30 
+        assert checkout_solution.checkout("ABCDABCDA") == 245   
+        
+    def test_invalid(self):
+        # test an invalid cart
+        assert checkout_solution.checkout("ABZCD") == -1
+
